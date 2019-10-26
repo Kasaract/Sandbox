1a3d80822862f9adc1128675dfa93332d3b29377
Kasaract <garynguyen2018@gmail.com>
2019-10-26 03:52:57 -0400

    First vue app

---
+++ Frameworks/first-vue-app/src/components/Todos.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div v-bind:key="todoItem.id" v-for="todoItem in todos">
+      <TodoItem v-bind:todo="todoItem" v-on:del-todo="$emit('del-todo', todoItem.id)" />
+    </div>
+  </div>
+</template>
+
+<script>
+import TodoItem from "./TodoItem";
+
+export default {
+  name: "Todos",
+  components: { TodoItem },
+  props: ["todos"]
+};
+</script>
+
+<style scoped>
+</style>
