d38fe40486a4424616652bd25c8fb7f63c7fddae
Kasaract <garynguyen2018@gmail.com>
2020-01-10 14:39:08 -0500

    Finish Ben Awad Typescript tutorial

--- Frameworks/Typescript-React/ben-awad-ts-react-ex/src/App.tsx
+++ Frameworks/Typescript-React/ben-awad-ts-react-ex/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import { TextField } from './TextField';
+// import { TextField } from './TextField';
+import { Counter } from './Counter';
 
 // props
 // hooks
@@ -9,7 +10,15 @@ import { TextField } from './TextField';
 const App: React.FC = () => {
   return (
     <div >
-      <TextField text='Hello' />
+      {/* <TextField text='Hello' handleChange={e => { }} /> */}
+      <Counter>
+        {({ count, setCount }) => (
+          <div>
+            {count}
+            <button onClick={() => setCount(count + 1)}>+</button>
+          </div>
+        )}
+      </Counter>
     </div>
   );
 }
